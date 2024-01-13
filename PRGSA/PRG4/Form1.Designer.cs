c6c6b6580daa3d7b6e9d8fe01e527e158011ab97
AWBoo <140343993+AWBoo@users.noreply.github.com>
2024-01-13 23:12:02 +0200

    Add files via upload
    Fun Uni assignment

---
+++ PRGSA/PRG4/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace PRG4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePickerDestination = new System.Windows.Forms.DateTimePicker();
+            this.textBoxPassengerName = new System.Windows.Forms.TextBox();
+            this.textBoxDestination = new System.Windows.Forms.TextBox();
+            this.textBoxTicketPrice = new System.Windows.Forms.TextBox();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBoxTicketIdToDelete = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBoxTicketIdToEdit = new System.Windows.Forms.TextBox();
+            this.textBoxUpdatedPassengerName = new System.Windows.Forms.TextBox();
+            this.textBoxUpdatedTicketPrice = new System.Windows.Forms.TextBox();
+            this.textBoxUpdatedDestination = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Passenger Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Destination";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ticket Price";
+            // 
+            // dateTimePickerDestination
+            // 
+            this.dateTimePickerDestination.Location = new System.Drawing.Point(40, 136);
+            this.dateTimePickerDestination.Name = "dateTimePickerDestination";
+            this.dateTimePickerDestination.Size = new System.Drawing.Size(347, 22);
+            this.dateTimePickerDestination.TabIndex = 3;
+            // 
+            // textBoxPassengerName
+            // 
+            this.textBoxPassengerName.Location = new System.Drawing.Point(216, 6);
+            this.textBoxPassengerName.Name = "textBoxPassengerName";
+            this.textBoxPassengerName.Size = new System.Drawing.Size(171, 22);
+            this.textBoxPassengerName.TabIndex = 4;
+            // 
+            // textBoxDestination
+            // 
+            this.textBoxDestination.Location = new System.Drawing.Point(216, 46);
+            this.textBoxDestination.Name = "textBoxDestination";
+            this.textBoxDestination.Size = new System.Drawing.Size(171, 22);
+            this.textBoxDestination.TabIndex = 5;
+            // 
+            // textBoxTicketPrice
+            // 
+            this.textBoxTicketPrice.Location = new System.Drawing.Point(216, 79);
+            this.textBoxTicketPrice.Name = "textBoxTicketPrice";
+            this.textBoxTicketPrice.Size = new System.Drawing.Size(171, 22);
+            this.textBoxTicketPrice.TabIndex = 6;
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(414, 6);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.RowHeadersWidth = 51;
+            this.dataGrid.RowTemplate.Height = 24;
+            this.dataGrid.Size = new System.Drawing.Size(815, 384);
+            this.dataGrid.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(216, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(171, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Refresh Grid";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(40, 240);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(149, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBoxTicketIdToDelete
+            // 
+            this.textBoxTicketIdToDelete.Location = new System.Drawing.Point(216, 240);
+            this.textBoxTicketIdToDelete.Name = "textBoxTicketIdToDelete";
+            this.textBoxTicketIdToDelete.Size = new System.Drawing.Size(171, 22);
+            this.textBoxTicketIdToDelete.TabIndex = 11;
+            this.textBoxTicketIdToDelete.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(40, 287);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(347, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Edit ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBoxTicketIdToEdit
+            // 
+            this.textBoxTicketIdToEdit.Location = new System.Drawing.Point(40, 324);
+            this.textBoxTicketIdToEdit.Name = "textBoxTicketIdToEdit";
+            this.textBoxTicketIdToEdit.Size = new System.Drawing.Size(149, 22);
+            this.textBoxTicketIdToEdit.TabIndex = 13;
+            // 
+            // textBoxUpdatedPassengerName
+            // 
+            this.textBoxUpdatedPassengerName.Location = new System.Drawing.Point(216, 323);
+            this.textBoxUpdatedPassengerName.Name = "textBoxUpdatedPassengerName";
+            this.textBoxUpdatedPassengerName.Size = new System.Drawing.Size(171, 22);
+            this.textBoxUpdatedPassengerName.TabIndex = 14;
+            // 
+            // textBoxUpdatedTicketPrice
+            // 
+            this.textBoxUpdatedTicketPrice.Location = new System.Drawing.Point(216, 368);
+            this.textBoxUpdatedTicketPrice.Name = "textBoxUpdatedTicketPrice";
+            this.textBoxUpdatedTicketPrice.Size = new System.Drawing.Size(171, 22);
+            this.textBoxUpdatedTicketPrice.TabIndex = 15;
+            // 
+            // textBoxUpdatedDestination
+            // 
+            this.textBoxUpdatedDestination.Location = new System.Drawing.Point(40, 368);
+            this.textBoxUpdatedDestination.Name = "textBoxUpdatedDestination";
+            this.textBoxUpdatedDestination.Size = new System.Drawing.Size(149, 22);
+            this.textBoxUpdatedDestination.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1157, 423);
+            this.Controls.Add(this.textBoxUpdatedDestination);
+            this.Controls.Add(this.textBoxUpdatedTicketPrice);
+            this.Controls.Add(this.textBoxUpdatedPassengerName);
+            this.Controls.Add(this.textBoxTicketIdToEdit);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBoxTicketIdToDelete);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.textBoxTicketPrice);
+            this.Controls.Add(this.textBoxDestination);
+            this.Controls.Add(this.textBoxPassengerName);
+            this.Controls.Add(this.dateTimePickerDestination);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDestination;
+        private System.Windows.Forms.TextBox textBoxPassengerName;
+        private System.Windows.Forms.TextBox textBoxDestination;
+        private System.Windows.Forms.TextBox textBoxTicketPrice;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBoxTicketIdToDelete;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBoxTicketIdToEdit;
+        private System.Windows.Forms.TextBox textBoxUpdatedPassengerName;
+        private System.Windows.Forms.TextBox textBoxUpdatedTicketPrice;
+        private System.Windows.Forms.TextBox textBoxUpdatedDestination;
+    }
+}
+
---
+++ PRGSA/PRG4/Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace PRG4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Load += Form1_Load;
+        }
+
+        private void SetupDataGridView()
+        {
+            dataGrid.Columns[4].DefaultCellStyle.Format = "F2"; // Format as fixed-point with 2 decimal places
+        }
+
+
+        private void RefreshData()
+        {
+            DataTable yourDataTable = GetYourData();
+
+            if (yourDataTable != null)
+            {
+                dataGrid.DataSource = yourDataTable;
+
+                SetupDataGridView();
+            }
+
+
+            textBoxTicketIdToEdit.Text = "(TicketID)";
+            textBoxUpdatedPassengerName.Text = "(Passenger Name)";
+            textBoxUpdatedDestination.Text = "(Destination Text)";
+            textBoxUpdatedTicketPrice.Text = "(Ticket Price)";
+            textBoxTicketIdToDelete.Text = "(Ticket ID To Delete)";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Call RefreshData on form load to display data initially
+            RefreshData();
+        }
+
+        private string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Asher\\source\\repos\\PRG4\\PRG4\\BusTicketDB.mdf;Integrated Security=True";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string passengerName = textBoxPassengerName.Text;
+                string destination = textBoxDestination.Text;
+                DateTime date = dateTimePickerDestination.Value;
+
+
+                if (!decimal.TryParse(textBoxTicketPrice.Text, out decimal ticketPrice))
+                {
+                    MessageBox.Show("Invalid ticket price. Please enter a valid number.");
+                    return;
+                }
+
+                BookTicket(passengerName, destination, ticketPrice, date);
+
+                // Optional: Clear the textboxes after booking
+                textBoxPassengerName.Clear();
+                textBoxDestination.Clear();
+                textBoxTicketPrice.Clear();
+
+                RefreshData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error booking ticket: " + ex.Message);
+            }
+        }
+
+        private void BookTicket(string passengerName, string destination, decimal ticketPrice, DateTime date)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Generate a random TicketId
+                    Random random = new Random();
+                    int ticketId = random.Next(1000, 9999); // Adjust the range as needed
+
+                    // Omit TicketId in the query; it will be set manually
+                    string query = "INSERT INTO BookedTickets (TicketId, PassengerName, Destination, Date, TicketPrice) " +
+                                   "VALUES (@TicketId, @PassengerName, @Destination, @Date, @TicketPrice);";
+
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    // Set the parameters before executing the query
+                    command.Parameters.AddWithValue("@TicketId", ticketId);
+                    command.Parameters.AddWithValue("@PassengerName", passengerName);
+                    command.Parameters.AddWithValue("@Destination", destination);
+                    command.Parameters.AddWithValue("@Date", date);
+
+                    command.Parameters.AddWithValue("@TicketPrice", ticketPrice);
+
+                    // Execute the query
+                    command.ExecuteNonQuery();
+
+                    MessageBox.Show($"Ticket {ticketId} booked successfully!");
+                }
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error booking ticket: " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+                DataTable yourDataTable = GetYourData();
+
+                // Set the data source for dataGridView1 to display the entire table
+                dataGrid.DataSource = yourDataTable;
+
+        }
+
+        private DataTable GetYourData()
+        {
+            DataTable dataTable = new DataTable("BookedTickets");
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "SELECT * FROM BookedTickets";
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(query, connection))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions appropriately in your application
+                MessageBox.Show("Error: " + ex.Message);
+                return null; // Ensure a non-null DataTable is returned
+            }
+
+            return dataTable;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get the TicketId from the user
+                if (!int.TryParse(textBoxTicketIdToDelete.Text, out int ticketIdToDelete))
+                {
+                    MessageBox.Show("Invalid Ticket ID. Please enter a valid number.");
+                    return;
+                }
+
+                DeleteTicket(ticketIdToDelete);
+
+                // Optional: Clear the TextBox after deleting
+                textBoxTicketIdToDelete.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error deleting ticket: " + ex.Message);
+            }
+        }
+
+        private void DeleteTicket(int ticketId)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Specify the DELETE query with a WHERE clause based on TicketId
+                    string query = "DELETE FROM BookedTickets WHERE TicketId = @TicketId;";
+
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    // Set the parameter for TicketId before executing the query
+                    command.Parameters.AddWithValue("@TicketId", ticketId);
+
+                    // Execute the query
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show($"Ticket {ticketId} deleted successfully!");
+                        RefreshData();
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Ticket {ticketId} not found.");
+                    }
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error deleting ticket: " + ex.Message);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get the TicketId from the user 
+                if (!int.TryParse(textBoxTicketIdToEdit.Text, out int ticketIdToEdit))
+                {
+                    MessageBox.Show("Invalid Ticket ID. Please enter a valid number.");
+                    return;
+                }
+
+                // Get updated information from the user
+                string updatedPassengerName = textBoxUpdatedPassengerName.Text;
+                string updatedDestination = textBoxUpdatedDestination.Text;
+
+                if (!decimal.TryParse(textBoxUpdatedTicketPrice.Text, out decimal updatedTicketPrice))
+                {
+                    MessageBox.Show("Invalid updated ticket price. Please enter a valid number.");
+                    return;
+                }
+
+                EditTicket(ticketIdToEdit, updatedPassengerName, updatedDestination, updatedTicketPrice);
+
+                // Optional: Clear the TextBoxes after editing
+                textBoxTicketIdToEdit.Clear();
+                textBoxUpdatedPassengerName.Clear();
+                textBoxUpdatedDestination.Clear();
+                textBoxUpdatedTicketPrice.Clear();
+
+                RefreshData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error editing ticket: " + ex.Message);
+            }
+        }
+
+        private void EditTicket(int ticketId, string updatedPassengerName, string updatedDestination, decimal updatedTicketPrice)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Specify the UPDATE query with a WHERE clause based on TicketId
+                    string query = "UPDATE BookedTickets " +
+                                   "SET PassengerName = @UpdatedPassengerName, Destination = @UpdatedDestination, TicketPrice = @UpdatedTicketPrice " +
+                                   "WHERE TicketId = @TicketId;";
+
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    // Set the parameters for updated information and TicketId before executing the query
+                    command.Parameters.AddWithValue("@UpdatedPassengerName", updatedPassengerName);
+                    command.Parameters.AddWithValue("@UpdatedDestination", updatedDestination);
+                    command.Parameters.AddWithValue("@UpdatedTicketPrice", updatedTicketPrice);
+                    command.Parameters.AddWithValue("@TicketId", ticketId);
+
+                    // Execute the query
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show($"Ticket {ticketId} updated successfully!");
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Ticket {ticketId} not found.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error editing ticket: " + ex.Message);
+            }
+        }
+    }
+}
